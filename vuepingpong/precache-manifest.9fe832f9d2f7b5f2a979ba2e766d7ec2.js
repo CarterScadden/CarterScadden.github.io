3de52a2fd398a65461708f1bc37cef8557dcb25a
CarterScadden <carter@scaddenfamily.com>
2020-05-11 18:40:37 -0600

    added vuepingpong via file upload

---
+++ vuepingpong/precache-manifest.9fe832f9d2f7b5f2a979ba2e766d7ec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439b04d1a7438e7d375f",
+    "url": "vuepingpong/css/app.aae2bed0.css"
+  },
+  {
+    "revision": "4e9c40e6a1ac05dea433",
+    "url": "vuepingpong/css/chunk-vendors.089a7470.css"
+  },
+  {
+    "revision": "43ae8ae85db04dea1b1712c2d567938c",
+    "url": "vuepingpong/index.html"
+  },
+  {
+    "revision": "439b04d1a7438e7d375f",
+    "url": "vuepingpong/js/app.a205b7e5.js"
+  },
+  {
+    "revision": "4e9c40e6a1ac05dea433",
+    "url": "vuepingpong/js/chunk-vendors.87b0a365.js"
+  },
+  {
+    "revision": "27b1ab3fec5c4c6e5b08437fcc4becdc",
+    "url": "vuepingpong/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "vuepingpong/robots.txt"
+  }
+]);
